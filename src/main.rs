792a45313c0759a90e89e800adcc12f4183c914d
EternalDev1 <robinsond.dev@gmail.com>
2023-04-19 21:24:35 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,43 @@ use rand::Rng;
 
 fn main() {
     start_game();
-    let mut turns = 0;
-    let target_number = generate_number_between(1, 10);
-    let mut user_guess = String::new();
-    let mut game_over = false;
 
-    while turns < 10 && game_over != true {
-        if turns == 0 {
+    let mut user_turns = 1;
+    let mut user_guess = String::new();
+    let mut game_won = false;
+
+    let target_number = generate_number_between(1, 10);
+
+    while user_turns <= 10 && game_won != true {
+        if user_turns == 1 {
             println!("Please Enter a number between 1 and 10");
-        } else if turns != 0 && game_over == false {
+        } else if user_turns != 1 && game_won == false {
             println!("That was incorrect.Please try again!")
         }
 
-        stdin().read_line(&mut user_guess).expect("Failed to read user input");
-
-        turns += 1;
+        get_user_guess(&mut user_guess);
 
         if user_guess.trim() == target_number.to_string() {
-            game_over = true;
+            game_won = true;
+            break;
         }
-        // Add the end of each cycle reset user_guess as readline appends to existing var
-        reset_user_guess(&mut user_guess)
+        // At the end of each cycle reset user_guess as readline appends to existing var
+        reset_user_guess(&mut user_guess);
+
+        user_turns += 1;
     }
-    if game_over {
-        println!("I don't know how you did it but you are right the number is {} and you did it in only {}, turn(s)", target_number, turns);
+
+    if game_won && user_turns == 1 {
+        println!("I don't know how you did it but you are right the number is {} and you did it in only {}, turn", target_number, user_turns);
+    } else if game_won {
+        println!("You guessed {} the right number in {} turns, Congratulations!", target_number, user_turns);
     } else {
-        println!("I'm sorry the number was {}, not {}. Game Over in {} turn(s)!", target_number, user_guess, turns)
+        println!("I'm sorry you did not guess the number in {} turn(s) Game Over!", user_turns)
     }
+}
+
+fn get_user_guess(mut user_guess: &mut String) -> usize {
+    stdin().read_line(&mut user_guess).expect("Failed to read user input")
 }
 
 
